7a797e2f99988e5e7040f1e9fa40f35e855002a0
Taufiq Mohammed <taufiqmmhdz@gmail.com>
2018-03-11 15:39:51 +0800

    minor changes

--- main.py
+++ main.py
@@ -223,7 +223,7 @@ def render():
     global wedge_coords, cursor_coords, wedges_render
     screen.fill(white)
     label = font.render(solve_state, True, blue)
-    screen.blit(label, (screen_width/2 - label.get_width(),
+    screen.blit(label, (screen_width/2 - label.get_width()/2,
                         screen_height - (label.get_height() / 2 * 3)))
     score_font = font.render("Score: " + str(score), True, blue)
     screen.blit(score_font, (screen_width/2, screen_height/2))
